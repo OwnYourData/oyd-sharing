f6830e247236d0a4cdae38755a6616ac0830bc95
Gabriel Unterholzer <unterholzer.gabriel@gmail.com>
2022-05-29 22:29:58 +0200

    Added oydid

--- src/utils/did.ts
+++ src/utils/did.ts
@@ -1,20 +1,25 @@
 import { DIDDocument, Resolver } from 'did-resolver';
-import { getResolver } from 'web-did-resolver';
+import { getResolver as getWebResolver } from 'web-did-resolver';
+// @ts-expect-error no types available
+import { getResolver as getOydidResolver } from 'oydid-did-resolver';
 
 interface ServiceEndpoint {
   "@type": string;
   instance: string[];
 }
 
-const webResolver = getResolver();
-
 const didResolver = new Resolver({
-  ...webResolver,
+  ...getWebResolver(),
+  ...getOydidResolver(),
 });
 
 export const resolve = async (did: string) => didResolver.resolve(did);
 
 export const getServiceEndpoint = (document: DIDDocument, id: string): string | undefined => {
+  // TODO: bugfix
+  if ((document as any).didDocument)
+    document = (document as any).didDocument as DIDDocument;
+  
   const item = document.service?.find(x => x.id === id);
 
   return (item?.serviceEndpoint as unknown as ServiceEndpoint).instance[0];
